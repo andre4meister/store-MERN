c6eda32158d15550b36e1c8c9ce920b50d3736cd
andre4meister <51925104+andre4meister@users.noreply.github.com>
2023-02-21 14:56:23 +0200

    deleted build dir

--- backend/build/utils/parseURL.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (baseURL) => (req, res) => {
-    const parsedURL = new URL(req.url, baseURL);
-    const params = {};
-    parsedURL.searchParams.forEach((value, key) => (params[key] = value));
-    req.params = params;
-    req.pathname = parsedURL.pathname;
-};
